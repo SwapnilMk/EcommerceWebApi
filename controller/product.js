b07df070a912c20286daf915a4d0c456a997a727
SwapnilMk <mswapnil218@gmail.com>
2024-02-19 14:59:32 +0530

    Mongoose CURD

--- controller/product.js
+++ controller/product.js
@@ -7,51 +7,61 @@ const model = require('../model/product')
 const Product = model.Product
 
 
-exports.addProduct = async(req, res) => {
-    const product = new Product({ 
-        "id": 4,
-        "title": "iPhonssdasdasde 9",
-        "description": "Ansadsadsa apple mobile which is nothing like apple",
-        "price": 59});
-    await product.save();
-
-    console.log(product);
-
-    res.send('product added!')
-
+exports.addProduct = async (req, res) => {
     // const newProduct = products.push(req.body)
     // console.log(newProduct)
     // res.json(newProduct)
+    const product = new Product(req.body);
+    await product.save();
+    console.log(product);
+    res.send(req.body)
 }
 
-exports.getAllProducts = (req, res) => {
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find()
     res.json(products)
 }
 
-exports.getProduct = (req, res) => {
-    const id = req.params.id
-    const product = products.find(e => e.id == id)
+exports.getProduct = async (req, res) => {
+    const id = +req.params.id
+    // const product = products.find(e => e.id == id)
+    const product = await Product.findOne({ "id": id })
     res.send(product)
 }
 
-exports.replaceProduct = (req, res) => {
-    let id = req.params.id;
-    let index = products.findIndex(e => e.id == id);
-    const updatedProduct = products.splice(index, 1, { ...req.body, id: id })
-    res.json(updatedProduct)
+exports.replaceProduct = async (req, res) => {
+    let id = +req.params.id;
+    // let index = products.findIndex(e => e.id == id);
+    // const updatedProduct = products.splice(index, 1, { ...req.body, id: id })
+    try {
+        const product = await Product.findOneAndReplace({ 'id': id }, req.body, { new: true })
+        res.json(product)
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-exports.updateProduct = (req, res) => {
+exports.updateProduct = async (req, res) => {
     let id = req.params.id;
-    let index = products.findIndex(e => e.id == id);
-    const product = products[index]
-    const updatedProduct = products.splice(index, 1, { ...product, ...req.body, id: id })
-    res.json(updatedProduct)
+    // let index = products.findIndex(e => e.id == id);
+    // const product = products[index]
+    // const updatedProduct = products.splice(index, 1, { ...product, ...req.body, id: id })
+    try {
+        const product = await Product.findOneAndUpdate({ 'id': id }, { 'price': 999 }, { new: true })
+        res.json(product)
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-exports.deleteProduct = (req, res) => {
-    const id = req.params.id;
-    let index = data.findIndex((e) => e.id == id);
-    const deleteProduct = data.splice(index, 1);
-    res.json(deleteProduct);
+exports.deleteProduct = async (req, res) => {
+    const id = +req.params.id;
+    // let index = data.findIndex((e) => e.id == id);
+    // const deleteProduct = data.splice(index, 1);
+    try {
+        const product = await Product.findOneAndDelete({ 'id': id })
+        res.json(product);
+    } catch (err) {
+        console.log(err)
+    }
 }
